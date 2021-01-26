1e9e15c4062cdc2dd034017601c28c95302f5395
student <student@eda-bc-09.localdomain>
2021-01-27 10:50:47 +1300

    Add list of rank names

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 import Nav from './Nav'
 import Home from './Home'
@@ -6,12 +7,14 @@ import Home from './Home'
 function App () {
   return (
     <>
+    <Router>
       <h1>Navigating the taxonomic routes</h1>
       {/* This 'main' div is only for styling (so we can use flexbox) */}
       <div className='main'>
         <Nav />
-        <Home />
+        <Route path="/" exact component={Home} />
       </div>
+      </Router>
     </>
   )
 }
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ranks from '../../data/ranks'
+
 
 function Nav () {
+  const rankNames = Object.keys(ranks)
   return (
     <div>
       <h2>Nav</h2>
       <ul>
-        <li>Build your ranks list here</li>
+      {rankNames.map(ranks => {
+        return (
+          <li>
+            <Link to={`/list/${ranks}`}>{ranks}</Link>
+          </li>
+        )
+      })}
       </ul>
     </div>
   )
