2aba022ffa1c0718f3903e78a17964cc58b959d9
Tom May <73091215+tom-may@users.noreply.github.com>
2021-01-27 10:23:21 +1300

    initial commit

--- client/index.js
+++ client/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom'
 
 import App from './components/App'
 
+import { HashRouter as Router,Route } from 'react-router-dom'
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <>
+    <Router>
+    <App />
+    
+    </Router>
+    
+    </>,
     document.getElementById('app')
   )
 })
