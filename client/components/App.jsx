17ded666ff0c84d7b33d19e5dd4549a0f7b8ea4c
student <student@eda-bc-09.localdomain>
2021-01-27 12:31:34 +1300

    add list of ranks

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 
 import Nav from './Nav'
 import Home from './Home'
+import Classifications from './Classifications'
 
 function App () {
   return (
@@ -13,6 +14,8 @@ function App () {
       <div className='main'>
         <Nav />
         <Route path="/" exact component={Home} />
+        <Route path="/list/:ranks" exact component={Classifications} />
+        <Route path="/list/:ranks/:name" exact component={Classifications} />
       </div>
       </Router>
     </>
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -5,14 +5,15 @@ import ranks from '../../data/ranks'
 
 function Nav () {
   const rankNames = Object.keys(ranks)
+  console.log(rankNames)
   return (
     <div>
       <h2>Nav</h2>
       <ul>
-      {rankNames.map(ranks => {
+      {rankNames.map(rank => {
         return (
           <li>
-            <Link to={`/list/${ranks}`}>{ranks}</Link>
+            <Link to={`/list/${rank}`}>{rank}</Link>
           </li>
         )
       })}
