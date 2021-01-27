df1fa1fc38c6c533e43f2cf9ccfadfaad0ed2dcc
student <student@eda-bc-11.localdomain>
2021-01-27 15:17:12 +1300

    Managed to get the classifications displaying

--- client/components/Classifications.jsx
+++ client/components/Classifications.jsx
@@ -4,9 +4,26 @@ import rankData from '../../data/ranks'
 function classifications (props) {
   const rank = props.match.params.classification
   console.log(rankData[rank])
+  const newRank = rankData[rank]
   return (
     <>
-    <h3>Classifications!</h3>
+    <h3>Classifications</h3>
+    <ul>
+      {newRank.map(rank => {
+        console.log(rank)
+        return (
+          <li key={rank.id}>
+            {rank.name}
+            <br>
+            </br>
+            <br>
+            </br>
+            {rank.description} 
+          </li>
+
+        )
+      })}
+    </ul>
     </>
   )
 }
