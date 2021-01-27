1c32f6dcf4a54fb79b9622ea6d51febc08ca35e6
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-01-27 23:43:46 +1300

    Format Nav file

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -2,26 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import ranksData from "../../data/ranks.js"
 
-function Nav (props) {
+function Nav(props) {
   // console.log(Object.keys(ranksData))
-    return (
-      <nav>
-        <h2>Nav</h2>
-        <ul>
-
-              {Object.keys(ranksData).map(rank => {
-                return (
-                <Link key={rank} to={`/list/${rank}`}>
-                  <li>
-                    {rank}
-                  </li>
-                </Link>
-                )
-              })}
-
-        </ul>
-      </nav>
-    );
+  return (
+    <nav>
+      <h2>Nav</h2>
+      <ul>
+        {Object.keys(ranksData).map(rank => {
+          return (
+            <li key={rank}>
+              <Link to={`/list/${rank}`}>
+                {rank}
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  );
 
 };
 
